1c8f71b9925b0c086ca4762c18e7ead519fd78dc
kzhekshen <kzhekshen@incentient.com>
2018-03-27 22:31:46 +0600

    mail sender fixed

---
+++ functions/listeners/onCreateUser.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+const db = require('../db');
+// mail
+const sgMail = require('@sendgrid/mail');
+const SENDGRID_API_KEY = functions.config().mail.key;
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+exports.onCreateDistributor = functions.database.ref('/distributors-users/{disId}/{userId}')
+.onWrite(event => {
+  let data = event.data.val();
+  let { userId, disId } = event.params;
+
+  return db.getValue('/users/' + disId)
+    .then(distributor => {
+
+      console.log('data', data);
+      console.log('usserId = ', userId);
+      console.log('distributor = ', disId);
+
+      const msg = {
+        to: data.email,
+        from: distributor.email,
+        subject: 'Email verifacation for new user',
+        text: `Email from SaleZman`,
+        html: `<h1>Please notice</h1>
+        <p>${distributor.email} make you salezman on SaleZman Application</p>
+        <p>For login please use</p>
+        <p><b>Your email: ${data.email}</b></p>
+        <p><b>Your password by default: </b> 123456789</p>
+        <i>You can change your password on prodile settings on Application</i>
+        `
+      };
+      return sgMail.send(msg);
+  });
+});
